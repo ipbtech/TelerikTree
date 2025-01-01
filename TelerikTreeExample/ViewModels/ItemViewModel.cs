64f56779219fc360b21698a8deb19e80e6ae8adc
Петров Илья Вадимович <ipb.tech@yandex.ru>
2025-01-02 01:19:20 +0300

    fix bugs

--- TelerikTreeExample/ViewModels/ItemViewModel.cs
+++ TelerikTreeExample/ViewModels/ItemViewModel.cs
@@ -5,15 +5,19 @@ namespace TelerikTreeExample.ViewModels
     public class ItemViewModel
     {
         public int Id { get; set; }
+
+        [DataType("Integer")]
         public int? ParentId { get; set; }
         
         [Required]
         [Display(Name = "ItemName")]
         public string Name { get; set; }
 
+        [DataType("Decimal")]
         public double? Value { get; set; }
 
         [Display(Name = "ItemValue")]
+        [DataType("Decimal")]
         public double? AggregateValue { get; set; }
     }
 }
